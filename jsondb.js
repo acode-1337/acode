513f6797cb8f60eb5956ccc9b9d667f1476e631c
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-11-27 22:45:55 +0800

    added json db support

--- jsondb.js
+++ jsondb.js
@@ -1,2 +1,14 @@
+const path = require('path')
+const root = require('./root')
 const JSONdb = require('simple-json-db')
 
+/**
+ * Initialize new JSON db in root directory
+ *
+ * @param {String} filename filename without .json extension
+ * @example jsondb.initialize('main')
+ */
+const initialize = filename =>
+    new JSONdb(path.join(root, `${filename}.json`))
+
+module.exports = initialize
